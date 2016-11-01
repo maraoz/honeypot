a750eab5556669bdba05e00dfc73149db862d03d
Manuel Araoz <manuelaraoz@gmail.com>
2016-11-01 14:55:43 -0300

    initial release

---
+++ index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+var sys = require('sys')
+var exec = require('child_process').exec;
+
+function puts(error, stdout, stderr) { sys.puts(stdout) }
+
+app.get('/', function (req, res) {
+	exec("xdotool key Ctrl+alt+l", puts);
+});
+
+app.listen(3001, function () {
+  console.log('Honeypot activated');
+});
